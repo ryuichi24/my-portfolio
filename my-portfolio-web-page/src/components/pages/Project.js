0a1348c2c0efebcdc8d7d210efefcc6e552b3348
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-04-04 18:26:35 +0300

    add my portfolio web page in compose file

--- my-portfolio-web-page/src/components/pages/Project.js
+++ my-portfolio-web-page/src/components/pages/Project.js
@@ -16,12 +16,12 @@ const Project = () => {
                 <div className="project-item-container">
                     <div className="project-item">
                         <div className="project-item-image project-one">
-                             <h2 className="project-name">Time Table Manager</h2>
+                             <h2 className="project-name">Youtube Link Manager</h2>
                         </div>
                         <div className="project-item-detail">
-                            <p>Time table sharing app built with React/Interact with Api to manage data and authentication</p>
+                            <p>hello</p>
                             <div className="project-link-container">
-                                <a className="link project-link" href={'https://github.com/ryuichi24/time-table-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
+                                <a className="link project-link" href={'https://github.com/ryuichi24/video-link-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
                                 <span>|</span>
                                 <a className="link project-link" href={process.env.REACT_APP_PROJECT_URL_ONE}><FontAwesomeIcon className="font-awesome" icon={faLink}/>App</a>
                             </div>
@@ -30,24 +30,24 @@ const Project = () => {
 
                     <div className="project-item">
                         <div className="project-item-image project-two">
-                            <h2 className="project-name">Api For Time Table Manager</h2>
+                            <h2 className="project-name">Api For Youtube Link Manager</h2>
                         </div>
                         <div className="project-item-detail">
-                            <p>Api intereacting with MySQL database/JWT authentication</p>
+                            <p>hello</p>
                             <div className="project-link-container">
-                                <a className="link project-link" href={'https://github.com/ryuichi24/api-time-table-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
+                                <a className="link project-link" href={'https://github.com/ryuichi24/api-video-link-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
                             </div>
                         </div>
                     </div>
 
                     <div className="project-item">
                         <div className="project-item-image project-three">
-                             <h2 className="project-name">Youtube Link Managerr</h2>
+                             <h2 className="project-name">Time Table Manager</h2>
                         </div>
                         <div className="project-item-detail">
-                            <p>still in process</p>
+                            <p>Time table sharing app built with React/Interact with Api to manage data and authentication</p>
                             <div className="project-link-container">
-                                <a className="link project-link" href={'https://github.com/ryuichi24/video-link-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
+                                <a className="link project-link" href={'https://github.com/ryuichi24/time-table-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
                                 <span>|</span>
                                 <a className="link project-link" href={process.env.REACT_APP_PROJECT_URL_TWO}><FontAwesomeIcon className="font-awesome" icon={faLink}/>App</a>
                             </div>
@@ -56,12 +56,12 @@ const Project = () => {
 
                     <div className="project-item">
                         <div className="project-item-image project-four">
-                            <h2 className="project-name">Api For Youtube Link Manager</h2>
+                            <h2 className="project-name">Api For Time Table Manager</h2>
                         </div>
                         <div className="project-item-detail">
-                            <p>still in process</p>
+                            <p>Api intereacting with MySQL database/JWT authentication</p>
                             <div className="project-link-container">
-                                <a className="link project-link" href={'https://github.com/ryuichi24/api-video-link-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
+                                <a className="link project-link" href={'https://github.com/ryuichi24/api-time-table-manager'}><FontAwesomeIcon className="font-awesome" icon={faGithub}/>Github</a>
                             </div>
                         </div>
                     </div>

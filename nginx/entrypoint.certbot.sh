59a4014c86a51334b1be7837848acd5e19a049c9
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-04-07 17:35:35 +0300

    minor changes

--- nginx/entrypoint.certbot.sh
+++ nginx/entrypoint.certbot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-certbot certonly --standalone -d ryuichi24.com,portfolio.ryuichi24.com,viket.ryuichi24.com,table.ryuichi24.com --email ryuichi.nishi24@gmail.com -n --agree-tos --expand
+certbot certonly -n -d ryuichi24.com,portfolio.ryuichi24.com,viket.ryuichi24.com,table.ryuichi24.com \
+  --standalone --preferred-challenges http --email ryuichi.nishi24@gmail.com --agree-tos --expand
 
 
 # start cron

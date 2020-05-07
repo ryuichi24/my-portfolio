681f17c70d383cdbe30589b861b1b647d7892a33
ryuichi24 <ryuichi.nishi24@gmail.com>
2020-05-07 17:40:34 +0300

    add google tag manager by using gtm-module

--- my-portfolio-web-page/src/index.js
+++ my-portfolio-web-page/src/index.js
@@ -6,6 +6,14 @@ import App from './App';
 //import libraries
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-PBNDLGB'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 ReactDOM.render(
   <Router>
     <App />
